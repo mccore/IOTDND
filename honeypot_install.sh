0c07731683eb95e0fdd6f71f807475c6caa46933
mccore <edwardmccorry@aol.com>
2018-01-10 16:23:09 -0500

    added cron job to honeypot_install, added adduser and deluser commands to setup

--- honeypot_install.sh
+++ honeypot_install.sh
@@ -12,8 +12,9 @@ pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 sudo iptables -t nat -A PREROUTING -p tcp --dport 1022 -j REDIRECT --to-port 22
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
+(crontab -l ; echo "00 06 * * * python /root/report.py") | crontab - #List the current cron jobs, create a new one to run report.py at 6AM every day, and pipe all that into crontab
 #Need to change the honeypot config to allow the root login in the data/userdb.txt file (This will be solved by using fork of honeypot with my modifications)
 #Need to change the honeypot config to allow ssh_exec. This also needs testing as I need to limit the exec to honeypots only. Also, can the honeypots use ssh at all?
 #I also probably need to change the default honeypot config in order to configure the reporting mechanism. Maybe. Its possible that the json files are the best I can do. Maybe the SQL server can be set up else and added to by the honeypots
-#Create a cron job to startup the report.py file if the honeypot has no auto-report mechanism
-#This whole thing needs to be tested. It also assumes that apt-get can be used which is debian based. Also it assumes that wget is installed. If it isn't I will need to use busy box I think
+#Create a cron job to startup the report.py file if the honeypot has no auto-report mechanism (done)
+#This whole thing needs to be tested. It also assumes that apt-get can be used which is debian based. Also it assumes that wget is installed. If it isn't I will need to use busybox I think
--- setup.py
+++ setup.py
@@ -41,49 +41,65 @@ for line in real_output:
 		aHost = Host(anIP, aService, aUser, aPass)
 		hosts.append(aHost)
 
-def doSSH(host):
-		#I need to create a new user and password here as well. This needs to also be stored.
-		#The port change for administration is done in the honeypot install
-		#TODO: Create a new root user/pass combo
-		#TODO: Disable the root account and delete it if possible.
-		#TODO: Transfer the report.py file
+#I should make sure there aren't any duplicates in the hosts list. I should allow multiple of the same IPs but only one of each service.
 
-		host.processed = True
-		ssh_transfer_command = "cat honeypot_install.sh | sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'cat > honeypot_install.sh'".format(passwd=host.passwd, user=host.user, IP=host.IP)
-		ssh_transfer_process = subprocess.Popen(ssh_transfer_command, stdout=subprocess.PIPE, shell=True)
-		ssh_transfer_process.wait() #This wait ensures that the process finishes before we try to communicate. Else we break the pipe.
-		ssh_transfer_output, ssh_transfer_error = ssh_transfer_process.communicate()
+def doSSH(host, newuser, newpass):
+	#TODO: Store new user/pass combo
+	#TODO: Error checking
+	#TODO: Print statements to say whats going on
 
-		ssh_setup_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'chmod +x honeypot_install.sh && sudo ./honeypot_install.sh'".format(passwd=host.passwd, user=host.user, IP=host.IP)
-		ssh_setup_process = subprocess.Popen(ssh_setup_command, stdout=subprocess.PIPE, shell=True)
-		ssh_setup_process.wait()
-		ssh_setup_output, ssh_setup_error = ssh_setup_process.communicate()
+	host.processed = True
+	transfer_install_command = "cat honeypot_install.sh | sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'cat > honeypot_install.sh'".format(passwd=host.passwd, user=host.user, IP=host.IP)
+	transfer_install_process = subprocess.Popen(transfer_install_command, stdout=subprocess.PIPE, shell=True)
+	transfer_install_process.wait() #This wait ensures that the process finishes before we try to communicate. Else we break the pipe.
+	transfer_install_output, transfer_install_error = transfer_install_process.communicate()
+
+	transfer_report_command = "cat report.py | sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'cat > report.py'".format(passwd=host.passwd, user=host.user, IP=host.IP)
+	transfer_report_process = subprocess.Popen(transfer_report_command, stdout=subprocess.PIPE, shell=True)
+	transfer_report_process.wait() #This wait ensures that the process finishes before we try to communicate. Else we break the pipe.
+	transfer_report_output, transfer_report_error = transfer_report_process.communicate()
+
+	setup_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'chmod +x honeypot_install.sh && sudo ./honeypot_install.sh'".format(passwd=host.passwd, user=host.user, IP=host.IP)
+	setup_process = subprocess.Popen(setup_command, stdout=subprocess.PIPE, shell=True)
+	setup_process.wait()
+	setup_output, setup_error = setup_process.communicate()
+
+	newuser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'sudo adduser {newuser} --gecos "" --disabled-password ; echo '{newuser}:{newuserpassword}' | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, newuser=newuser, newuserpassword=newpass)
+	newuser_process = subprocess.Popen(newuser_command, stdout=subprocess.PIPE, shell=True)
+	newuser_process.wait()
+	newuser_output, newuser_error = newuser_process.communicate()
+
+	#deluser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} 'sudo passwd -l {user}'".format(passwd=host.passwd, user=host.user, IP=host.IP)
+	#deluser_process = subprocess.Popen(deluser_command, stdout=subprocess.PIPE, shell=True)
+	#deluser_process.wait()
+	#deluser_output, deluser_error = deluser_process.communicate()
 
 def doTelnet(host):
-		#The other end is going to need to use netcat which I think is a decent assumption
-		#Essentially here I am going to have the device setup ssh and disable telnet for security reasons. TinySSH might be best here.
-		#Once ssh is set up then the ssh configuration should take place
-		host.processed = True
-		telnet_transfer_command = ""
-		tn = telnetlib.Telnet(host.IP)
+	#The other end is going to need to use netcat which I think is a decent assumption
+	#Essentially here I am going to have the device setup ssh and disable telnet for security reasons. TinySSH might be best here.
+	#Once ssh is set up then the ssh configuration should take place
+	host.processed = True
+	telnet_transfer_command = ""
+	tn = telnetlib.Telnet(host.IP)
 
-		tn.read_until("login: ")
-		tn.write(host.user + "\n")
-		if password:
-			tn.read_until("Password: ")
-			tn.write(host.passwd + "\n")
+	tn.read_until("login: ")
+	tn.write(host.user + "\n")
+	if password:
+		tn.read_until("Password: ")
+		tn.write(host.passwd + "\n")
 
-		tn.write("ls\n")
-		tn.write("exit\n")
+	tn.write("ls\n")
+	tn.write("exit\n")
 
 #Now loop through the addresses and their respective protocol (telnet or ssh).
 print "Looping through hosts"
 for host in hosts:
 	#Run the honeypot setup script on the remote system.
 	#TODO: Need to make sure that if a host has the ability to use ssh then it is. Basically, telnet should be a last resort
-	#TODO: Put each of these if statements into their own function. This will make the telnet setup easier since it can just call the ssh setup after its done.
+	#TODO: Make the Telnet if statement call doSSH after doTelnet sets up the ssh client.
+	#TODO: Somehow doSSH needs to take in a new user and password. Perhaps this whole file should have arguments for one master user and password combo, procedural generation, or manual imput
 	if host.service == "[ssh]" and host.processed == False:
-		doSSH(host)
+		doSSH(host, "test", "test")
 
 	if host.service == "[telnet]" and host.processed == False:
 		doTelnet(host)
