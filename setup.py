21e94646a0c4ce7379bd5e412568b02bd36b98e2
mccore <edwardmccorry@aol.com>
2018-01-13 14:18:56 -0500

    I figured it out thank God. Now i can go back to actually developing

--- setup.py
+++ setup.py
@@ -72,19 +72,21 @@ def doSSH(host, newuser, newpass):
 	setup_process.wait()
 	setup_output, setup_error = setup_process.communicate()
 
-	print "{IP}: Adding new user {anewuser}".format(IP=host.IP, anewuser=newuser)
+	print "{IP}: Creating encrypted password".format(IP=host.IP)
+	#pass_command = "openssl passwd -crypt test"
+	pass_command = "mkpasswd -m sha-512 test"
+	pass_process = subprocess.Popen(pass_command, stdout=subprocess.PIPE, shell=True)
+	pass_process.wait()
+	pass_output, pass_error = pass_process.communicate()
+	passhash = re.sub(r"\$", "\\$", pass_output).rstrip()
+	print passhash
+
+	print "{IP}: Adding new user test".format(IP=host.IP)
 	#newuser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'sudo adduser --gecos "" --disabled-password {anewuser} && echo {anewuser}:{anewuserpassword} | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser, anewuserpassword=newpass)
-	newuser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'sudo adduser --gecos "" --disabled-password {anewuser}'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser)
+	newuser_command = '''sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} "sudo useradd -m -s /bin/bash -g sudo -p '{encpass}' test"'''.format(passwd=host.passwd, user=host.user, IP=host.IP, encpass=passhash)
 	newuser_process = subprocess.Popen(newuser_command, stdout=subprocess.PIPE, shell=True)
 	newuser_process.wait()
 	newuser_output, newuser_error = newuser_process.communicate()
-
-	print "{IP}: Changing new user {anewuser} password".format(IP=host.IP, anewuser=newuser)
-	#newpass_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'echo {anewuser}:{anewuserpassword} | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser, anewuserpassword=newpass)
-	newpass_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'echo {anewuser}:{anewuserpassword} | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser, anewuserpassword=newpass)
-	newpass_process = subprocess.Popen(newpass_command, stdout=subprocess.PIPE, shell=True)
-	newpass_process.wait()
-	newpass_output, newpass_error = newpass_process.communicate()
 
 	print "{IP}: Disabling old user".format(IP=host.IP)
 	deluser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'sudo passwd -l {user}'".format(passwd=host.passwd, user=host.user, IP=host.IP)
@@ -122,22 +124,7 @@ for host in hosts:
 	#TODO: Make the Telnet if statement call doSSH after doTelnet sets up the ssh client.
 	#TODO: Somehow doSSH needs to take in a new user and password. Perhaps this whole file should have arguments for one master user and password combo, procedural generation, or manual input
 	if host.service == "[ssh]" and host.processed == False:
-		#doSSH(host, "test", "test")
-		print "{IP}: Creating encrypted password".format(IP=host.IP)
-		#pass_command = "openssl passwd -crypt test"
-		pass_command = "mkpasswd -m sha-512 test"
-		pass_process = subprocess.Popen(pass_command, stdout=subprocess.PIPE, shell=True)
-		pass_process.wait()
-		pass_output, pass_error = pass_process.communicate()
-		passhash = re.sub(r"\$", "\\$", pass_output).rstrip()
-		print passhash
-
-		print "{IP}: Adding new user test".format(IP=host.IP)
-		#newuser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'sudo adduser --gecos "" --disabled-password {anewuser} && echo {anewuser}:{anewuserpassword} | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser, anewuserpassword=newpass)
-		newuser_command = '''sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} "sudo useradd -m -s /bin/bash -g sudo -p '{encpass}' test"'''.format(passwd=host.passwd, user=host.user, IP=host.IP, encpass=passhash)
-		newuser_process = subprocess.Popen(newuser_command, stdout=subprocess.PIPE, shell=True)
-		newuser_process.wait()
-		newuser_output, newuser_error = newuser_process.communicate()
+		doSSH(host, "test", "test")
 
 		# print "{IP}: Adding new user test".format(IP=host.IP)
 		# #newuser_command = "sshpass -p {passwd} ssh -o StrictHostKeyChecking=no {user}@{IP} -p 1022 'sudo adduser --gecos "" --disabled-password {anewuser} && echo {anewuser}:{anewuserpassword} | sudo chpasswd'".format(passwd=host.passwd, user=host.user, IP=host.IP, anewuser=newuser, anewuserpassword=newpass)
