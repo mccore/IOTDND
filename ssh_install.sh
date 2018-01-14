510fe1a91d1dabc8d391e183dc5a12b92fdd87b9
mccore <edwardmccorry@aol.com>
2018-01-14 14:14:08 -0500

    TinySSH isn't going to work out because of its compilation requirements

--- ssh_install.sh
+++ ssh_install.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-wget https://mojzis.com/software/tinyssh/tinyssh-20180110.tar.gz
-wget https://mojzis.com/software/tinyssh/tinyssh-20180110.tar.gz.asc
-gpg --verify tinyssh-20180110.tar.gz.asc tinyssh-20180110.tar.gz
-gunzip < tinyssh-20180110.tar.gz | tar -xf -
-cd tinyssh-20180110
-make
-sudo make install
-sudo tinysshd-makekey /etc/tinyssh/sshkeydir
-sudo echo "ssh stream tcp nowait root /usr/sbin/tinysshd tinysshd -l -v /etc/tinyssh/sshkeydir" >> /etc/inetd.conf
-sudo /etc/init.d/xinetd restart
+wget https://matt.ucc.asn.au/dropbear/releases/dropbear-2017.75.tar.bz2
+tar xpf dropbear-2017.75.tar.bz2
+cd dropbear-2017.75/
+./configure
+make install
