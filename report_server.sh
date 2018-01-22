306cebe1fb661fb4fd0c8944ab5b460c658d576b
mccore <edwardmccorry@aol.com>
2018-01-22 12:41:45 -0500

    Running the local server in a while loop may be a bad idea

--- report_server.sh
+++ report_server.sh
@@ -4,12 +4,12 @@ if [ ! -d $PATH/logs/ ]; then
 	mkdir -p $PATH/logs/
 fi
 
-while true; do
-	nc -lvp 3333 2> $PATH/status 1> $PATH/cowrie.json;
-	ip=$(cat -A status | cut -d'[' -f 3 | cut -d']' -f 1);
-	date=$(date '+%Y-%m-%d-%H:%M');
-	if [ ! -d $PATH/logs/"$(echo $ip)"_logs/ ]; then
-	  mkdir -p $PATH/logs/"$(echo $ip)"_logs/;
-	fi
-	mv $PATH/cowrie.json $PATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
-done
+#while true; do
+nc -lkvp 3333 2> $PATH/status 1> $PATH/cowrie.json;
+ip=$(cat -A $PATH/status | cut -d'[' -f 3 | cut -d']' -f 1);
+date=$(date '+%Y-%m-%d-%H:%M');
+if [ ! -d $PATH/logs/"$(echo $ip)"_logs/ ]; then
+  mkdir -p $PATH/logs/"$(echo $ip)"_logs/;
+fi
+mv $PATH/cowrie.json $PATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
+#done
