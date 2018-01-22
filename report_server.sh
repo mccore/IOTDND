edf93e1f0c0ccc0598b5fcc197ce818b5d506939
mccore <edwardmccorry@aol.com>
2018-01-22 12:58:05 -0500

    Looks like the while loop for the reporting is necessary. I think I made the listen call blocking which was what I was missing

--- report_server.sh
+++ report_server.sh
@@ -4,12 +4,12 @@ if [ ! -d $DIRPATH/logs/ ]; then
 	mkdir -p $DIRPATH/logs/
 fi
 
-#while true; do
-nc -lkvp 3333 2> $DIRPATH/status 1> $DIRPATH/cowrie.json;
-ip=$(cat -A $DIRPATH/status | cut -d'[' -f 3 | cut -d']' -f 1);
-date=$(date '+%Y-%m-%d-%H:%M');
-if [ ! -d $DIRPATH/logs/"$(echo $ip)"_logs/ ]; then
-  mkdir -p $DIRPATH/logs/"$(echo $ip)"_logs/;
-fi
-mv $DIRPATH/cowrie.json $DIRPATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
-#done
+while true; do
+	nc -lkvp 3333 2> $DIRPATH/status 1> $DIRPATH/cowrie.json;
+	ip=$(cat -A $DIRPATH/status | cut -d'[' -f 3 | cut -d']' -f 1);
+	date=$(date '+%Y-%m-%d-%H:%M');
+	if [ ! -d $DIRPATH/logs/"$(echo $ip)"_logs/ ]; then
+	  mkdir -p $DIRPATH/logs/"$(echo $ip)"_logs/;
+	fi
+	mv $DIRPATH/cowrie.json $DIRPATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
+done
