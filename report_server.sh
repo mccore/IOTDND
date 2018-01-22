174c0cf9c04306a044a9c0bfcb2a83f6ce5092c9
mccore <edwardmccorry@aol.com>
2018-01-22 12:47:37 -0500

    I totally forgot that PATH is an env variable so I needed to name it something different in the report script

--- report_server.sh
+++ report_server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-PATH=$1
-if [ ! -d $PATH/logs/ ]; then
-	mkdir -p $PATH/logs/
+DIRPATH=$1
+if [ ! -d $DIRPATH/logs/ ]; then
+	mkdir -p $DIRPATH/logs/
 fi
 
 #while true; do
-nc -lkvp 3333 2> $PATH/status 1> $PATH/cowrie.json;
-ip=$(cat -A $PATH/status | cut -d'[' -f 3 | cut -d']' -f 1);
+nc -lkvp 3333 2> $DIRPATH/status 1> $DIRPATH/cowrie.json;
+ip=$(cat -A $DIRPATH/status | cut -d'[' -f 3 | cut -d']' -f 1);
 date=$(date '+%Y-%m-%d-%H:%M');
-if [ ! -d $PATH/logs/"$(echo $ip)"_logs/ ]; then
-  mkdir -p $PATH/logs/"$(echo $ip)"_logs/;
+if [ ! -d $DIRPATH/logs/"$(echo $ip)"_logs/ ]; then
+  mkdir -p $DIRPATH/logs/"$(echo $ip)"_logs/;
 fi
-mv $PATH/cowrie.json $PATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
+mv $DIRPATH/cowrie.json $DIRPATH/logs/"$(echo $ip)"_logs/"$(echo $date)"_cowrie.json;
 #done
