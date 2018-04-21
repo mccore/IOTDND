edf3b670caf87a8be2a77a1dab7bcd526a91d68b
mccore <edwardmccorry@aol.com>
2018-04-21 16:06:30 -0400

    Some changes need to be made to accomadate Fedora

--- honeypot_install.sh
+++ honeypot_install.sh
@@ -4,7 +4,7 @@ if [ -x "$(command -v apt-get)" ]; then
 	sudo apt-get -y install python-virtualenv libssl-dev libffi-dev build-essential libpython-dev python2.7-minimal
 fi
 if [ -x "$(command -v yum)" ]; then
-	sudo yum install -y python-virtualenv openssl-devel libffi-devel make automake gcc gcc-c++ kernel-devel libpython-devel python27 python2-virtualenv
+	sudo yum install -y openssl-devel libffi-devel make automake gcc gcc-c++ kernel-devel libpython-devel python27 python27-devel python27-pip python27-setuptools python27-tools python27-virtualenv
 	sudo /usr/bin/easy_install virtualenv
 fi
 #sudo adduser --gecos "" --disabled-password cowrie && sudo su - cowrie << EOF
@@ -22,14 +22,17 @@ EOF
 sudo iptables -t nat -A PREROUTING -p tcp --dport 1022 -j REDIRECT --to-port 22
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
 sudo iptables -A INPUT -p tcp -m tcp --dport 23 -j DROP
-(crontab -u $newuser -l ; echo "@reboot sudo iptables -t nat -A PREROUTING -p tcp --dport 1022 -j REDIRECT --to-port 22 ; sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222 ; sudo iptables -A INPUT -p tcp -m tcp --dport 23 -j DROP") | crontab -u $newuser -
-(crontab -u $newuser -l ; echo "@reboot su - cowrie -c '~/cowrie/bin/cowrie start'") | crontab -u $newuser -
 if [ -x "$(command -v apt-get)" ]; then
+	(crontab -u $newuser -l ; echo "@reboot sudo iptables -t nat -A PREROUTING -p tcp --dport 1022 -j REDIRECT --to-port 22 ; sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222 ; sudo iptables -A INPUT -p tcp -m tcp --dport 23 -j DROP") | crontab -u $newuser -
+	(crontab -u $newuser -l ; echo "@reboot su - cowrie -c '~/cowrie/bin/cowrie start'") | crontab -u $newuser -
 	sudo apt-get -y update
 fi
 if [ -x "$(command -v yum)" ]; then
+	(crontab -u $newuser -l ; echo "*/1 * * * * sudo iptables -t nat -A PREROUTING -p tcp --dport 1022 -j REDIRECT --to-port 22 ; sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222 ; sudo iptables -A INPUT -p tcp -m tcp --dport 23 -j DROP") | crontab -u $newuser -
+	(crontab -u $newuser -l ; echo "*/1 * * * * su - cowrie -c '~/cowrie/bin/cowrie start'") | crontab -u $newuser -
 	sudo yum update -y
 fi
 
 #I need to add if statements to check for the commands that I use. If it doesn't have them then I install busybox.
 #Test on Fedora
+#The current problem is that the source cowrie line or the virtual env line isnt working
